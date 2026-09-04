0ecfdef9fd3c84edff58afee7e5746d8292eb38f
itimchenko00-hash <itimchenko00@gmail.com>
2026-09-04 19:15:26 +0300

    ALADIN: route working page directly to six-block implementation

--- ЭТАЛОН-02/MMW-COMPANY/src/aladin-sendfile-hook.js
+++ ЭТАЛОН-02/MMW-COMPANY/src/aladin-sendfile-hook.js
@@ -1,10 +1,12 @@
 const express=require('express');
 const fs=require('fs');
+const path=require('path');
 const originalSendFile=express.response.sendFile;
 express.response.sendFile=function(filePath,options,callback){
   if(typeof filePath==='string'&&filePath.includes('aladin-presentation-suite.html')){
-    fs.readFile(filePath,'utf8',(err,body)=>{
-      if(err){if(typeof callback==='function')return callback(err);return this.status(500).send('ALADIN file error')}
+    const working=path.join(path.dirname(filePath),'aladin-working-v2.html');
+    fs.readFile(working,'utf8',(err,body)=>{
+      if(err){if(typeof callback==='function')return callback(err);return this.status(500).send('ALADIN working page error')}
       this.type('html');
       return this.send(body);
     });
