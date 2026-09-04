4e93f93f11a0a5f0d3ba87b43c425c000dbf5591
itimchenko00-hash <itimchenko00@gmail.com>
2026-09-04 14:46:03 +0300

    ALADIN: activate removal of deprecated process blocks

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path=require('path');
 const ROOT=path.join(__dirname,'ЭТАЛОН-02','MMW-COMPANY','src');
+require(path.join(ROOT,'aladin-cleanup-hook.js'));
 require(path.join(ROOT,'final-cleanup-hook.js'));
 require(path.join(ROOT,'products-cart-hook.js'));
 require(path.join(ROOT,'order-catalog-hook.js'));
