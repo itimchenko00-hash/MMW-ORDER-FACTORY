5ea9e6712754fb652be8cf62cd650f910212f2da
itimchenko00-hash <itimchenko00@gmail.com>
2026-09-04 14:13:36 +0300

    FACTORY: finalize protected exact ETALON-01 and ETALON-02 copies

--- ЭТАЛОН-01/server.js
+++
@@ -1,8 +0,0 @@
-const path=require('path');
-const ROOT=path.join(__dirname,'ЭТАЛОН-02','MMW-COMPANY','src');
-require(path.join(ROOT,'final-cleanup-hook.js'));
-require(path.join(ROOT,'products-cart-hook.js'));
-require(path.join(ROOT,'order-catalog-hook.js'));
-require(path.join(ROOT,'company-ui-hook.js'));
-require(path.join(ROOT,'labels-cleanup-hook.js'));
-require(path.join(ROOT,'server.js'));
