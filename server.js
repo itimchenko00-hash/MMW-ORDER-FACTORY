3ada736bd9ff2ea9a11551accedbcba5dc883ca1
itimchenko00-hash <itimchenko00@gmail.com>
2026-09-04 23:17:25 +0300

    ALADIN FACTORY: recover canonical publication server

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ if(!fs.existsSync(URBAN_OVERRIDE)){console.error('[ALADIN-FATAL] Missing COMPACT
 if(!fs.existsSync(SCANDINAVIAN_OVERRIDE)){console.error('[ALADIN-FATAL] Missing SCANDINAVIAN override:',SCANDINAVIAN_OVERRIDE);process.exit(1)}
 if(!fs.existsSync(BRICK_OVERRIDE)){console.error('[ALADIN-FATAL] Missing CONTEMPORARY BRICK override:',BRICK_OVERRIDE);process.exit(1)}
 app.disable('x-powered-by');
-app.use((req,res,next)=>{res.set('Cache-Control','no-store,no-cache,must-revalidate,proxy-revalidate,max-age=0');res.set('Pragma','no-cache');res.set('Expires','0');res.set('X-ALADIN-SOURCE','FACTORY-CANONICAL');res.set('X-ALADIN-REPO','itimchenko00-hash/MMW-ORDER-FACTORY');res.set('X-ALADIN-BRANCH','aladin-six-blocks-working');res.set('X-ALADIN-BUILD','ALADIN-CONTEMPORARY-BRICK-2026-09-04');next()});
+app.use((req,res,next)=>{res.set('Cache-Control','no-store,no-cache,must-revalidate,proxy-revalidate,max-age=0');res.set('Pragma','no-cache');res.set('Expires','0');res.set('X-ALADIN-SOURCE','FACTORY-CANONICAL');res.set('X-ALADIN-REPO','itimchenko00-hash/MMW-ORDER-FACTORY');res.set('X-ALADIN-BRANCH','aladin-six-blocks-working');res.set('X-ALADIN-BUILD','ALADIN-RECOVERY-2026-09-04');next()});
 app.get('/aladin',(req,res)=>{let html=fs.readFileSync(ALADIN,'utf8');const urban=fs.readFileSync(URBAN_OVERRIDE,'utf8');const scandinavian=fs.readFileSync(SCANDINAVIAN_OVERRIDE,'utf8');const brick=fs.readFileSync(BRICK_OVERRIDE,'utf8');html=html.replace('</body>','<script>'+urban+'\n'+scandinavian+'\n'+brick+'</script></body>');res.type('html').send(html)});
 app.get('/',(req,res)=>res.redirect('/aladin'));
 app.use((req,res)=>res.status(404).send('ALADIN route not found'));
