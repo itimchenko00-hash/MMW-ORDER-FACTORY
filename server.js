5b6aafd3ae4ffc06b8dd90bc7ef3e028aedac22b
itimchenko00-hash <itimchenko00@gmail.com>
2026-09-06 20:45:07 +0300

    CONSTITUTION: remove inert global hook wrappers

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ require(path.join(ROOT,'nexus-work-enhancement-hook.js'));
 require(path.join(ROOT,'nexus-work-catalog-hook.js'));
 require(path.join(ROOT,'nexus-work-catalog-media-hook.js'));
 require(path.join(ROOT,'final-cleanup-hook.js'));
-require(path.join(ROOT,'products-cart-hook.js'));
-require(path.join(ROOT,'order-catalog-hook.js'));
 require(path.join(ROOT,'company-ui-hook.js'));
 require(path.join(ROOT,'labels-cleanup-hook.js'));
 require(path.join(ROOT,'unified-process-system-hook.js'));
