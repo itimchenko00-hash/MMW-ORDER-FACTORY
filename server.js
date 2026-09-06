11d141ba77f9648e10e5be5b19cc2bca94aaf64e
itimchenko00-hash <itimchenko00@gmail.com>
2026-09-07 01:33:10 +0300

    NEXUS WORK: activate logical presentation order in Factory deployment

--- server.js
+++ server.js
@@ -9,4 +9,5 @@ require(path.join(ROOT,'unified-process-system-hook.js'));
 require(path.join(ROOT,'language-switcher-hook.js'));
 require(path.join(ROOT,'aladin-investor-owner-engine-hook.js'));
 require(path.join(ROOT,'nexus-work-catalog-photo-hook.js'));
+require(path.join(ROOT,'nexus-work-unified-hook.js'));
 require(path.join(ROOT,'server.js'));
