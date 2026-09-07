4fccf7321937880c1ab6bd5c489606a81e600875
itimchenko00-hash <itimchenko00@gmail.com>
2026-09-07 16:59:26 +0300

    AGROHUB: publish MMW-COMPANY catalog to Factory

--- server.js
+++ server.js
@@ -10,4 +10,5 @@ require(path.join(ROOT,'language-switcher-hook.js'));
 require(path.join(ROOT,'aladin-investor-owner-engine-hook.js'));
 require(path.join(ROOT,'nexus-work-catalog-photo-hook.js'));
 require(path.join(ROOT,'nexus-work-unified-hook.js'));
+require(path.join(ROOT,'agrohub-catalog-hook.js'));
 require(path.join(ROOT,'server.js'));
